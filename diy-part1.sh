ff9f9a409e7f616bdeaf400406a8d7a4a787b3e9
jswolf <Skywol@qq.com>
2021-10-06 17:06:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/CCnut/feed-netkeeper.git -b LUCI-LUA-UCITRACK package/netkeeper
